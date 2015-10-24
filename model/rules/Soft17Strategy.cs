754e689fa390183bc7d2b3d72ff2d1c89eca042d
mj223dg <mj223dg@student.lnu.se>
2015-10-24 15:11:33 +0200

    working on the soft 17 rule

---
+++ model/rules/Soft17Strategy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.model.rules
+{
+    class Soft17Straegy : IHitStrategy
+    {
+        private const int g_hitLimit = 17;
+
+        public bool DoHit(model.Player a_dealer)
+        {
+            if (a_dealer.CalcScore() == g_hitLimit)
+            {
+
+            }
+            return a_dealer.CalcScore() < g_hitLimit;
+        }
+    }
+}
